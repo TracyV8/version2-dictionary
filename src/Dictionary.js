cc7b661d688eb1e7f2e1577ca104ea103596807e
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-07-12 16:12:16 +0800

    results and meanings sections added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    // console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -27,6 +30,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
