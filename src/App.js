0c2337d6fe643379135608df00bdc31bfd183563
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-07-12 14:54:52 +0800

    Added basic form structure for Dictionary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> 📚 Dictionary 📚</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1> 📚 Dictionary 📚</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          {" "}
+          This App was coded by Tracy Vonk and is{" "}
+          <a
+            href="https://github.com/TracyV8/version2-dictionary"
+            target="_blank"
+            rel="nonreferrer"
+          >
+            {" "}
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
